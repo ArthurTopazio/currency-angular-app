b4cd7bd342a9be206ba4e6f255e705409a965914
ArthurTopazio <perereacmb@gmail.com>
2022-07-22 03:09:45 +0300

    first fetch

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -11,9 +11,31 @@ export class CardComponent implements OnInit {
 
   number = 42
 
+  inputHandler(event: any) {
+    console.log(event.target.value)
+    const val = event.target.value
+    this.title = val
+  }
+
+  changeTitle() {
+    this.title = 'new title'
+  }
+
+  disabled = false
+
+  currenc = null
+
+  takeKurs() {
+    fetch('https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11')
+      .then(response => response.json())
+      .then(commits => { alert(JSON.stringify(commits)); this.currenc = commits; console.log(this.currenc) });
+  }
+
   ngOnInit() {
-
-
+    setTimeout(() => {
+      this.imgUrl = 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b6/Twilight_%282008_film%29_poster.jpg/220px-Twilight_%282008_film%29_poster.jpg'
+      this.disabled = true
+    }, 3000)
   }
 
   imgUrl = 'http://www.calendarulmagic.ro/wp-content/uploads/2013/11/mickey.jpg'
