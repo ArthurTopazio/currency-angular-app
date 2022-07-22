55d7a4fec0a3da712322bdd5778efab8238cb249
ArthurTopazio <perereacmb@gmail.com>
2022-07-22 15:34:57 +0300

    converter updates

--- src/app/converter/converter.component.ts
+++ src/app/converter/converter.component.ts
@@ -14,6 +14,8 @@ export class ConverterComponent implements OnInit {
         this.usdIndex = commits['conversion_rates']['USD']
         this.eurIndex = commits['conversion_rates']['EUR']
         this.uahIndex = commits['conversion_rates']['UAH']
+        this.usdRate = +(this.uahIndex / this.usdIndex).toFixed(2)
+        this.eurRate = +(this.uahIndex / (this.usdIndex * this.eurIndex)).toFixed(2)
       });
   }
 
@@ -38,12 +40,23 @@ export class ConverterComponent implements OnInit {
     },
   }
 
-  //convertFunc = (metric1: any, val: any, metric2: any): any => (val * this.methods[metric1].index / this.methods[metric2].index).toFixed(2)
-
+  // convertFunc = (metric1: any, val: any, metric2: any): any => (val * metr1 / metr2).toFixed(2)
+  usdRate = 0
+  eurRate = 0
   imgUrl = 'https://d2v9ipibika81v.cloudfront.net/uploads/sites/67/20220113bemb1140x440.jpg'
 
+  inputHandler1(event: any) {
+    console.log(event.target.value)
+    const val = event.target.value
+  }
+
+  inputHandler2(event: any) {
+    console.log(event.target.value)
+    const val = event.target.value
+  }
+
   ngOnInit(): void {
-    this.getCrossIndex()
+    //  this.getCrossIndex()
   }
 
 }
